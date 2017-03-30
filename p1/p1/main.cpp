5095bddd168391ac1e91a228a39efc9b91fef362
FuYJ <zz28234830@gmail.com>
2017-03-30 15:14:08 +0800

    2

--- p1/p1/main.cpp
+++ p1/p1/main.cpp
@@ -7,12 +7,21 @@ using namespace std;
 char ch;
 deque<char> squre;
 deque<string> element;
+string result;
 
 int main(){
     
     while(1){
         ch = getchar();
-        if()
+        if(ch == ']'){
+        
+        }
+        else if(ch == '['){
+        
+        }
+        else{
+            
+        }
     }
     
 }
